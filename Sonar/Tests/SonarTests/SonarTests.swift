40c0c2c0b7aa4a0263851100f170d71b7c8d3045
Callum McColl <callum.mccoll@griffithuni.edu.au>
2022-05-29 10:47:14 +1000

    Updated tests to namespace databases in folders

--- Sonar/Tests/SonarTests/SonarTests.swift
+++ Sonar/Tests/SonarTests/SonarTests.swift
@@ -70,66 +70,156 @@ import SwiftfsmWBWrappers
 
 class SonarTests: XCTestCase {
     
+    final class InMemoryStore: MutableKripkeStructure {
+
+        let identifier: String
+
+        private var latestId: Int64 = 0
+
+        private var ids: [KripkeStatePropertyList: Int64] = [:]
+
+        var allStates: [Int64: (KripkeStatePropertyList, Bool, Set<KripkeEdge>)] = [:]
+
+        var acceptingStates: AnySequence<KripkeState> {
+            AnySequence(states.filter { $0.edges.isEmpty })
+        }
+
+        var initialStates: AnySequence<KripkeState> {
+            AnySequence(states.filter { $0.isInitial })
+        }
+
+        var states: AnySequence<KripkeState> {
+            AnySequence(allStates.keys.map {
+                try! self.state(for: $0)
+            })
+        }
+
+        init(identifier: String, states: Set<KripkeState>) {
+            self.identifier = identifier
+            for state in states {
+                let (id, _) = try! self.add(state.properties, isInitial: state.isInitial)
+                for edge in state.edges {
+                    try! self.add(edge: edge, to: id)
+                }
+            }
+        }
+
+        func add(_ propertyList: KripkeStatePropertyList, isInitial: Bool) throws -> (Int64, KripkeState) {
+            let id = try id(for: propertyList)
+            if nil == allStates[id] {
+                allStates[id] = (propertyList, isInitial, [])
+            }
+            return try (id, state(for: id))
+        }
+
+        func add(edge: KripkeEdge, to id: Int64) throws {
+            allStates[id]?.2.insert(edge)
+        }
+
+        func exists(_ propertyList: KripkeStatePropertyList) throws -> Bool {
+            return nil != ids[propertyList]
+        }
+
+        func data(for propertyList: KripkeStatePropertyList) throws -> (Int64, KripkeState) {
+            let id = try id(for: propertyList)
+            return try (id, state(for: id))
+        }
+
+        func id(for propertyList: KripkeStatePropertyList) throws -> Int64 {
+            if let id = ids[propertyList] {
+                return id
+            }
+            let id = latestId
+            latestId += 1
+            ids[propertyList] = id
+            return id
+        }
+
+        func state(for id: Int64) throws -> KripkeState {
+            guard let (plist, isInitial, edges) = allStates[id] else {
+                fatalError("State does not exist")
+            }
+            let state = KripkeState(isInitial: isInitial, properties: plist)
+            for edge in edges {
+                state.addEdge(edge)
+            }
+            return state
+        }
+
+
+    }
+
     final class TestableViewFactory: KripkeStructureViewFactory {
-        
+
         private let make: (String) -> TestableView
-        
+
         private(set) var createdViews: [TestableView] = []
-        
+
         var lastView: TestableView! {
             createdViews.last!
         }
-        
+
         init(make: @escaping (String) -> TestableView) {
             self.make = make
         }
-        
+
         func make(identifier: String) -> TestableView {
             let view = self.make(identifier)
             createdViews.append(view)
             return view
         }
-        
+
+        func outputViews(name: String) {
+            let filemanager = FileManager.default
+            let currentDirectory = URL(fileURLWithPath: filemanager.currentDirectoryPath, isDirectory: true)
+            let buildDirectory = currentDirectory.appendingPathComponent("kripke_structures", isDirectory: true)
+            let testDirectory = buildDirectory.appendingPathComponent(name, isDirectory: true)
+            defer {
+                filemanager.changeCurrentDirectoryPath(currentDirectory.path)
+            }
+            _ = try? filemanager.removeItem(atPath: testDirectory.path)
+            guard
+                let _ = try? filemanager.createDirectory(at: testDirectory, withIntermediateDirectories: true),
+                true == filemanager.changeCurrentDirectoryPath(testDirectory.path)
+            else {
+                fatalError("Unable to create views directory")
+            }
+            for view in createdViews {
+                let outputView = GraphVizKripkeStructureView(filename: view.identifier + ".gv")
+                let nusmvView = NuSMVKripkeStructureView(identifier: view.identifier)
+                try! outputView.generate(store: view.store, usingClocks: true)
+                try! nusmvView.generate(store: view.store, usingClocks: true)
+            }
+        }
+
     }
-    
+
     final class TestableView: KripkeStructureView {
-        
+
         typealias State = KripkeState
-        
+
         let identifier: String
-        
+
         let expectedIdentifier: String
-        
+
         var expected: Set<KripkeState>
-        
-        private(set) var commits: [KripkeState] = []
-        
+
+        private(set) var store: KripkeStructure! = nil
+
         private(set) var result: Set<KripkeState>
-        
-        private(set) var finishCalled: Bool = false
-        
+
         init(identifier: String, expectedIdentifier: String, expected: Set<KripkeState>) {
             self.identifier = identifier
             self.expectedIdentifier = expectedIdentifier
             self.expected = expected
             self.result = Set<KripkeState>(minimumCapacity: expected.count)
         }
-        
-        func commit(state: KripkeState) {
-            commits.append(state)
-            result.insert(state)
+
+        func generate(store: KripkeStructure, usingClocks: Bool) throws {
+            self.store = store
+            self.result = try Set(store.states)
         }
 
-        func finish() {
-            finishCalled = true
-        }
-
-        func reset(usingClocks: Bool) {
-            commits.removeAll(keepingCapacity: true)
-            result.removeAll(keepingCapacity: true)
-            finishCalled = false
-        }
-        
         @discardableResult
         func check(readableName: String) -> Bool {
             XCTAssertEqual(result, expected)
@@ -137,11 +227,9 @@ class SonarTests: XCTestCase {
                 explain(name: readableName + "_")
             }
             XCTAssertEqual(identifier, expectedIdentifier)
-            XCTAssertEqual(commits.count, result.count) // Make sure all states are only ever committed once.
-            XCTAssertTrue(finishCalled)
-            return identifier == expectedIdentifier && result == expected && finishCalled
+            return identifier == expectedIdentifier && result == expected
         }
-        
+
         func explain(name: String = "") {
             guard expected != result else {
                 return
@@ -150,22 +238,15 @@ class SonarTests: XCTestCase {
             print("missing results: \(missingElements)")
             let extraneousElements = result.subtracting(expected)
             print("extraneous results: \(extraneousElements)")
-            let expectedView = GraphVizKripkeStructureView<KripkeState>(filename: "\(name)expected.gv")
-            expectedView.reset(usingClocks: true)
-            let resultView = GraphVizKripkeStructureView<KripkeState>(filename: "\(name)result.gv")
-            resultView.reset(usingClocks: true)
-            for state in expected.sorted(by: { $0.properties.description < $1.properties.description }) {
-                expectedView.commit(state: state)
-            }
-            for state in result.sorted(by: { $0.properties.description < $1.properties.description }) {
-                resultView.commit(state: state)
-            }
-            expectedView.finish()
-            resultView.finish()
+            let expectedView = GraphVizKripkeStructureView(filename: "\(name)expected.gv")
+            let resultView = GraphVizKripkeStructureView(filename: "\(name)result.gv")
+            let expectedStore = InMemoryStore(identifier: expectedIdentifier, states: expected)
+            try! expectedView.generate(store: expectedStore, usingClocks: true)
+            try! resultView.generate(store: store, usingClocks: true)
             print("Writing expected to: \(FileManager.default.currentDirectoryPath)/\(name)expected.gv")
             print("Writing result to: \(FileManager.default.currentDirectoryPath)/\(name)result.gv")
         }
-        
+
     }
     
     var readableName: String {
@@ -247,20 +328,21 @@ class SonarTests: XCTestCase {
             }
             return TestableView(identifier: name, expectedIdentifier: name, expected: [])
         }
-        verifier.verify(gateway: gateway, timer: clock, viewFactory: viewFactory, cycleDetector: HashTableCycleDetector())
-        let counts = viewFactory.createdViews.map(\.commits.count)
+        let factory = SQLiteKripkeStructureFactory(savingInDirectory: "/tmp/swiftfsm/\(readableName)")
+        do {
+            try verifier.verify(gateway: gateway, timer: clock, factory: factory).forEach {
+                try viewFactory.make(identifier: $0.identifier).generate(store: $0, usingClocks: true)
+            }
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+        let counts = viewFactory.createdViews.map(\.result.count)
         print(counts)
         for (index, view) in viewFactory.createdViews.enumerated() {
-            let outputView = GraphVizKripkeStructureView<KripkeState>(filename: "\(wbVars[index].0).gv")
-            let nusmvView = NuSMVKripkeStructureView<KripkeState>(identifier: "\(wbVars[index].0)")
-            outputView.reset(usingClocks: true)
-            nusmvView.reset(usingClocks: true)
-            for state in view.result.sorted(by: { $0.properties.description < $1.properties.description }) {
-                outputView.commit(state: state)
-                nusmvView.commit(state: state)
-            }
-            outputView.finish()
-            nusmvView.finish()
+            let outputView = GraphVizKripkeStructureView(filename: "\(wbVars[index].0).gv")
+            let nusmvView = NuSMVKripkeStructureView(identifier: "\(wbVars[index].0)")
+            try! outputView.generate(store: view.store, usingClocks: true)
+            try! nusmvView.generate(store: view.store, usingClocks: true)
         }
     }
     
@@ -335,20 +417,21 @@ class SonarTests: XCTestCase {
             }
             return TestableView(identifier: name, expectedIdentifier: name, expected: [])
         }
-        verifier.verify(gateway: gateway, timer: clock, viewFactory: viewFactory, cycleDetector: HashTableCycleDetector())
-        let counts = viewFactory.createdViews.map(\.commits.count)
+        let factory = SQLiteKripkeStructureFactory(savingInDirectory: "/tmp/swiftfsm/\(readableName)")
+        do {
+            try verifier.verify(gateway: gateway, timer: clock, factory: factory).forEach {
+                try viewFactory.make(identifier: $0.identifier).generate(store: $0, usingClocks: true)
+            }
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+        let counts = viewFactory.createdViews.map(\.result.count)
         print(counts)
         for (index, view) in viewFactory.createdViews.enumerated() {
-            let outputView = GraphVizKripkeStructureView<KripkeState>(filename: "\(wbVars[index].0).gv")
-            let nusmvView = NuSMVKripkeStructureView<KripkeState>(identifier: "\(wbVars[index].0)")
-            outputView.reset(usingClocks: true)
-            nusmvView.reset(usingClocks: true)
-            for state in view.result.sorted(by: { $0.properties.description < $1.properties.description }) {
-                outputView.commit(state: state)
-                nusmvView.commit(state: state)
-            }
-            outputView.finish()
-            nusmvView.finish()
+            let outputView = GraphVizKripkeStructureView(filename: "\(wbVars[index].0).gv")
+            let nusmvView = NuSMVKripkeStructureView(identifier: "\(wbVars[index].0)")
+            try! outputView.generate(store: view.store, usingClocks: true)
+            try! nusmvView.generate(store: view.store, usingClocks: true)
         }
     }
     
@@ -414,27 +497,14 @@ class SonarTests: XCTestCase {
                 cycleLength: UInt(wbVars.count) * timeslotLength
             )
         )
-        let viewFactory = TestableViewFactory { name in
-            guard nil != wbVars.first(where: { $0.0 == name}) else {
-                XCTFail("Unexepected view name: \(name)")
-                return TestableView(identifier: name, expectedIdentifier: "", expected: [])
+        let viewFactory = GraphVizKripkeStructureViewFactory()
+        let factory = SQLiteKripkeStructureFactory(savingInDirectory: "/tmp/swiftfsm/\(readableName)")
+        do {
+            try verifier.verify(gateway: gateway, timer: clock, factory: factory).forEach {
+                try viewFactory.make(identifier: $0.identifier).generate(store: $0, usingClocks: true)
             }
-            return TestableView(identifier: name, expectedIdentifier: name, expected: [])
-        }
-        verifier.verify(gateway: gateway, timer: clock, viewFactory: viewFactory, cycleDetector: HashTableCycleDetector())
-        let counts = viewFactory.createdViews.map(\.commits.count)
-        print(counts)
-        for (index, view) in viewFactory.createdViews.enumerated() {
-            let outputView = GraphVizKripkeStructureView<KripkeState>(filename: "\(wbVars[index].0).gv")
-            let nusmvView = NuSMVKripkeStructureView<KripkeState>(identifier: "\(wbVars[index].0)")
-            outputView.reset(usingClocks: true)
-            nusmvView.reset(usingClocks: true)
-            for state in view.result.sorted(by: { $0.properties.description < $1.properties.description }) {
-                outputView.commit(state: state)
-                nusmvView.commit(state: state)
-            }
-            outputView.finish()
-            nusmvView.finish()
+        } catch {
+            XCTFail(error.localizedDescription)
         }
     }
 
